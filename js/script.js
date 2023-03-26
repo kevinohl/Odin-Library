67de8b811f15f600484f0c236eb8da01f122fbf0
Kevin Ohl <kevinohl@msn.com>
2023-03-26 19:09:00 +0200

    Complete initial layout and styling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const myLibrary = [];
+const table = document.querySelector("#book-table");
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -5,7 +8,27 @@ function Book(title, author, pages, read) {
   this.read = read;
   this.info = function () {
     return `${title} by ${author}, ${pages} pages, ${
-      read ? 'already read.' : 'not read yet.'
+      read ? "already read." : "not read yet."
     }`;
   };
+}
+
+const book1 = new Book("titleee", "not me", "420", false);
+const book2 = new Book("titleex", "also not me", "69", true);
+myLibrary.push(book1);
+myLibrary.push(book2);
+
+function addBookToLibrary() {
+  myLibrary.forEach((book) => {
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      <td>${book.read}</td>
+      <td><button class="toggle-button">Toggle Read</button></td>
+      <td><button class="del-button">Delete</button></td>
+    `;
+    table.appendChild(newRow);
+  });
 }
