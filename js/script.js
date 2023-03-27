882db813ba8cee481ea32f35fdf1d579261bdbe2
Kevin Ohl <kevinohl@msn.com>
2023-03-27 05:48:13 +0200

    Implement adding and removing books

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const myLibrary = [];
+const mainContainer = document.querySelector(".main-container");
 const table = document.querySelector("#book-table");
+const addButton = document.querySelector("#add-button");
+const submitButton = document.querySelector("#book-submit");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -18,7 +21,25 @@ const book2 = new Book("titleex", "also not me", "69", true);
 myLibrary.push(book1);
 myLibrary.push(book2);
 
-function addBookToLibrary() {
+function renderLibrary() {
+  function toggleRead() {
+    const currentTitle =
+      this.parentNode.parentNode.querySelector("td:first-of-type").textContent;
+    const currentBook = myLibrary.find((book) => book.title === currentTitle);
+    currentBook.read = !currentBook.read;
+    renderLibrary();
+  }
+  function deleteBook() {
+    const currentTitle =
+      this.parentNode.parentNode.querySelector("td:first-of-type").textContent;
+    const currentBookIndex = myLibrary.findIndex(
+      (book) => book.title === currentTitle
+    );
+    myLibrary.splice(currentBookIndex, 1);
+    renderLibrary();
+  }
+  const currentEntries = document.querySelectorAll("tr td");
+  currentEntries.forEach((line) => line.remove());
   myLibrary.forEach((book) => {
     const newRow = document.createElement("tr");
     newRow.innerHTML = `
@@ -30,5 +51,48 @@ function addBookToLibrary() {
       <td><button class="del-button">Delete</button></td>
     `;
     table.appendChild(newRow);
+    const toggleButtons = document.querySelectorAll(".toggle-button");
+    toggleButtons.forEach((button) =>
+      button.addEventListener("click", toggleRead)
+    );
+    const deleteButtons = document.querySelectorAll(".del-button");
+    deleteButtons.forEach((button) =>
+      button.addEventListener("click", deleteBook)
+    );
   });
 }
+
+function toggleForm() {
+  const form = document.querySelector(".form-container");
+  const background = document.querySelector(".main-container");
+  form.classList.toggle("hidden");
+  background.classList.toggle("darkened");
+  if (!form.classList.contains("hidden")) {
+    mainContainer.addEventListener("click", toggleForm);
+  } else mainContainer.removeEventListener("click", toggleForm);
+}
+
+addButton.addEventListener("click", (e) => {
+  toggleForm();
+  e.stopPropagation();
+});
+
+function submitBook(event) {
+  event.preventDefault();
+  const bookForm = document.getElementById("book-form");
+  const bookTitle = document.getElementById("title").value;
+  const bookAuthor = document.getElementById("author").value;
+  const bookPages = document.getElementById("pages").value;
+  const bookRead = document.getElementById("read").checked;
+  if (bookTitle && bookAuthor && bookPages) {
+    toggleForm();
+    const newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+    bookForm.reset();
+    myLibrary.push(newBook);
+    renderLibrary();
+  }
+}
+
+submitButton.addEventListener("click", submitBook);
+
+renderLibrary();
